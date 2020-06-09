e303f9728d0f8361801e372cf55a203a1aed74b1
Laurent Cozic <laurent@cozic.net>
2020-06-09 19:37:13 +0100

    Moving theme under /lib

---
+++ ReactNativeClient/lib/themes/light.js
@@ -0,0 +1,44 @@
+// This is the default theme in Joplin
+const lightStyle = {
+	appearance: 'light',
+
+	backgroundColor: '#ffffff',
+	backgroundColorTransparent: 'rgba(255,255,255,0.9)',
+	oddBackgroundColor: '#dddddd',
+	color: '#222222', // For regular text
+	colorError: 'red',
+	colorWarn: '#9A5B00',
+	colorFaded: '#777777', // For less important text
+	colorBright: '#000000', // For important text
+	dividerColor: '#dddddd',
+	selectedColor: '#e5e5e5',
+	urlColor: '#155BDA',
+
+	backgroundColor2: '#162B3D',
+	depthColor: 'rgb(100, 182, 253, OPACITY)',
+	color2: '#f5f5f5',
+	selectedColor2: '#0269C2',
+	colorError2: '#ff6c6c',
+
+	raisedBackgroundColor: '#e5e5e5',
+	raisedColor: '#222222',
+
+	warningBackgroundColor: '#FFD08D',
+
+	tableBackgroundColor: 'rgb(247, 247, 247)',
+	codeBackgroundColor: 'rgb(243, 243, 243)',
+	codeBorderColor: 'rgb(220, 220, 220)',
+	codeColor: 'rgb(0,0,0)',
+
+	aceEditorTheme: 'chrome',
+	codeMirrorTheme: 'default',
+	codeThemeCss: 'atom-one-light.css',
+
+	// raisedBackgroundColor = '#0F2051'; // BG 2
+	// raisedColor = '#0F2051'; // COLOR 2
+	// raisedHighlightedColor = '#ffffff'; // COLOR BRIGHT 2
+	// textSelectionColor = '#00AEFF';
+
+};
+
+module.exports = lightStyle;
